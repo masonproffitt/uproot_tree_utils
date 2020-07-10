e254bdd45c84502cacf63c9860634327da6e24d3
Mason Proffitt <mproffit@cern.ch>
2020-07-11 00:15:16 +0200

    allow selections/skimming in cloning

--- tests/test_clone.py
+++ tests/test_clone.py
@@ -10,7 +10,7 @@ import uproot
 from uproot_tree_utils import clone_tree
 
 
-def test_clone_scalars():
+def test_scalars():
     original_file = uproot.open('tests/scalars_tree_file.root')
     tree_name = 'tree'
     original_tree = original_file[tree_name]
@@ -29,7 +29,7 @@ def test_clone_scalars():
             os.remove(new_file_name)
 
 
-def test_clone_vectors():
+def test_vectors():
     original_file = uproot.open('tests/vectors_tree_file.root')
     tree_name = 'tree'
     original_tree = original_file[tree_name]
@@ -38,10 +38,45 @@ def test_clone_vectors():
         clone_tree(original_tree, new_file_name)
         new_file = uproot.open(new_file_name)
         new_tree = new_file[tree_name]
-        assert new_tree['int_vector_branch'].array().tolist() == [[], [-1, 2]]
-        assert new_tree['long_vector_branch'].array().tolist() == [[], [-3, 4]]
-        assert abs(new_tree['float_vector_branch'].array() - awkward.fromiter([[], [-5.5, 6.6]])).max().max() < 1e-5
-        assert abs(new_tree['double_vector_branch'].array() - awkward.fromiter([[], [-7.7, 8.8]])).max().max() < 1e-5
+        assert new_tree['int_vector_branch'].array().tolist() == [[], [-1, 2, 3], [13]]
+        assert abs(new_tree['float_vector_branch'].array() - awkward.fromiter([[], [-7.7, 8.8, 9.9], [15.15]])).max().max() < 1e-5
+        assert abs(new_tree['double_vector_branch'].array() - awkward.fromiter([[], [-10.10, 11.11, 12.12], [16.16]])).max().max() < 1e-5
+    finally:
+        if os.path.isfile(new_file_name):
+            os.remove(new_file_name)
+
+
+def test_event_selection_scalars():
+    original_file = uproot.open('tests/scalars_tree_file.root')
+    tree_name = 'tree'
+    original_tree = original_file[tree_name]
+    new_file_name = tempfile.mkstemp(suffix='.root', dir=os.getcwd())[1]
+    try:
+        clone_tree(original_tree, new_file_name, selection=[False, True])
+        new_file = uproot.open(new_file_name)
+        new_tree = new_file[tree_name]
+        assert new_tree['int_branch'].array().tolist() == [-1]
+        assert new_tree['long_branch'].array().tolist() == [-2]
+        assert np.allclose(new_tree['float_branch'].array(), [3.3])
+        assert np.allclose(new_tree['double_branch'].array(), [4.4])
+        assert new_tree['bool_branch'].array().tolist() == [True]
+    finally:
+        if os.path.isfile(new_file_name):
+            os.remove(new_file_name)
+
+
+def test_event_selection_vectors():
+    original_file = uproot.open('tests/vectors_tree_file.root')
+    tree_name = 'tree'
+    original_tree = original_file[tree_name]
+    new_file_name = tempfile.mkstemp(suffix='.root', dir=os.getcwd())[1]
+    try:
+        clone_tree(original_tree, new_file_name, selection=[True, False, True])
+        new_file = uproot.open(new_file_name)
+        new_tree = new_file[tree_name]
+        assert new_tree['int_vector_branch'].array().tolist() == [[], [13]]
+        assert abs(new_tree['float_vector_branch'].array() - awkward.fromiter([[], [15.15]])).max().max() < 1e-5
+        assert abs(new_tree['double_vector_branch'].array() - awkward.fromiter([[], [16.16]])).max().max() < 1e-5
     finally:
         if os.path.isfile(new_file_name):
             os.remove(new_file_name)
--- uproot_tree_utils/clone.py
+++ uproot_tree_utils/clone.py
@@ -3,7 +3,7 @@ import numpy as np
 import uproot
 
 
-def clone_tree(tree, new_file_name, new_tree_name=None):
+def clone_tree(tree, new_file_name, new_tree_name=None, selection=None):
     if new_tree_name is None:
         new_tree_name = tree.name.decode('utf-8')
     branch_definition_dictionary = dict()
@@ -12,16 +12,22 @@ def clone_tree(tree, new_file_name, new_tree_name=None):
         name = branch.name.decode('utf-8')
         if isinstance(branch.interpretation.type, np.dtype):
             branch_definition_dictionary[name] = uproot.newbranch(branch.interpretation.type)
-            branch_content_dictionary[name] = branch.array()
         elif isinstance(branch.interpretation.content.type, np.dtype):
             size_name = name + '_n'
             while bytes(size_name, 'utf-8') in tree.keys():
                 size_name += '0'
             branch_definition_dictionary[name] = uproot.newbranch(branch.interpretation.content.type, size=size_name)
-            branch_content_dictionary[name] = branch.array()
-            branch_content_dictionary[size_name] = branch.array().count()
+            if selection is not None:
+                branch_content_dictionary[size_name] = branch.array().count()[selection]
+            else:
+                branch_content_dictionary[size_name] = branch.array().count()
         else:
             raise NotImplementedError('Branch type (' + str(branch.interpretation) + ') not supported')
+        if selection is not None:
+            content = branch.array()[selection]
+        else:
+            content = branch.array()
+        branch_content_dictionary[name] = content
     with uproot.recreate(new_file_name) as new_file:
         new_file[new_tree_name] = uproot.newtree(branch_definition_dictionary)
         new_file[new_tree_name].extend(branch_content_dictionary)
