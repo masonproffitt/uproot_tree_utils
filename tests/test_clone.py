e1946cd2ebb96b09457ab1de9a342fed6f979589
Mason Proffitt <mproffit@cern.ch>
2020-08-12 18:45:11 +0200

    remove underscores in variable names #11
    Use 'filename', 'treename', and 'branchname' (no underscore) for better consistency with ROOT

--- tests/test_clone.py
+++ tests/test_clone.py
@@ -12,86 +12,86 @@ from uproot_tree_utils import clone_tree
 
 def test_scalars():
     original_file = uproot.open('tests/scalars_tree_file.root')
-    tree_name = 'tree'
-    original_tree = original_file[tree_name]
-    new_file_name = tempfile.mkstemp(suffix='.root', dir=os.getcwd())[1]
+    treename = 'tree'
+    original_tree = original_file[treename]
+    new_filename = tempfile.mkstemp(suffix='.root', dir=os.getcwd())[1]
     try:
-        clone_tree(original_tree, new_file_name)
-        new_file = uproot.open(new_file_name)
-        new_tree = new_file[tree_name]
+        clone_tree(original_tree, new_filename)
+        new_file = uproot.open(new_filename)
+        new_tree = new_file[treename]
         assert new_tree['int_branch'].array().tolist() == [0, -1]
         assert new_tree['long_branch'].array().tolist() == [0, -2]
         assert np.allclose(new_tree['float_branch'].array(), [0.0, 3.3])
         assert np.allclose(new_tree['double_branch'].array(), [0.0, 4.4])
         assert new_tree['bool_branch'].array().tolist() == [False, True]
     finally:
-        if os.path.isfile(new_file_name):
-            os.remove(new_file_name)
+        if os.path.isfile(new_filename):
+            os.remove(new_filename)
 
 
 def test_vectors():
     original_file = uproot.open('tests/vectors_tree_file.root')
-    tree_name = 'tree'
-    original_tree = original_file[tree_name]
-    new_file_name = tempfile.mkstemp(suffix='.root', dir=os.getcwd())[1]
+    treename = 'tree'
+    original_tree = original_file[treename]
+    new_filename = tempfile.mkstemp(suffix='.root', dir=os.getcwd())[1]
     try:
-        clone_tree(original_tree, new_file_name)
-        new_file = uproot.open(new_file_name)
-        new_tree = new_file[tree_name]
+        clone_tree(original_tree, new_filename)
+        new_file = uproot.open(new_filename)
+        new_tree = new_file[treename]
         assert new_tree['int_vector_branch'].array().tolist() == [[], [-1, 2, 3], [13]]
         assert abs(new_tree['float_vector_branch'].array() - awkward.fromiter([[], [-7.7, 8.8, 9.9], [15.15]])).max().max() < 1e-5
         assert abs(new_tree['double_vector_branch'].array() - awkward.fromiter([[], [-10.10, 11.11, 12.12], [16.16]])).max().max() < 1e-5
     finally:
-        if os.path.isfile(new_file_name):
-            os.remove(new_file_name)
+        if os.path.isfile(new_filename):
+            os.remove(new_filename)
 
 
 def test_event_selection_scalars():
     original_file = uproot.open('tests/scalars_tree_file.root')
-    tree_name = 'tree'
-    original_tree = original_file[tree_name]
-    new_file_name = tempfile.mkstemp(suffix='.root', dir=os.getcwd())[1]
+    treename = 'tree'
+    original_tree = original_file[treename]
+    new_filename = tempfile.mkstemp(suffix='.root', dir=os.getcwd())[1]
     try:
-        clone_tree(original_tree, new_file_name, selection=[False, True])
-        new_file = uproot.open(new_file_name)
-        new_tree = new_file[tree_name]
+        clone_tree(original_tree, new_filename, selection=[False, True])
+        new_file = uproot.open(new_filename)
+        new_tree = new_file[treename]
         assert new_tree['int_branch'].array().tolist() == [-1]
         assert new_tree['long_branch'].array().tolist() == [-2]
         assert np.allclose(new_tree['float_branch'].array(), [3.3])
         assert np.allclose(new_tree['double_branch'].array(), [4.4])
         assert new_tree['bool_branch'].array().tolist() == [True]
     finally:
-        if os.path.isfile(new_file_name):
-            os.remove(new_file_name)
+        if os.path.isfile(new_filename):
+            os.remove(new_filename)
 
 
 def test_event_selection_vectors():
     original_file = uproot.open('tests/vectors_tree_file.root')
-    tree_name = 'tree'
-    original_tree = original_file[tree_name]
-    new_file_name = tempfile.mkstemp(suffix='.root', dir=os.getcwd())[1]
+    treename = 'tree'
+    original_tree = original_file[treename]
+    new_filename = tempfile.mkstemp(suffix='.root', dir=os.getcwd())[1]
     try:
-        clone_tree(original_tree, new_file_name, selection=[True, False, True])
-        new_file = uproot.open(new_file_name)
-        new_tree = new_file[tree_name]
+        clone_tree(original_tree, new_filename, selection=[True, False, True])
+        new_file = uproot.open(new_filename)
+        new_tree = new_file[treename]
         assert new_tree['int_vector_branch'].array().tolist() == [[], [13]]
         assert abs(new_tree['float_vector_branch'].array() - awkward.fromiter([[], [15.15]])).max().max() < 1e-5
         assert abs(new_tree['double_vector_branch'].array() - awkward.fromiter([[], [16.16]])).max().max() < 1e-5
     finally:
-        if os.path.isfile(new_file_name):
-            os.remove(new_file_name)
+        if os.path.isfile(new_filename):
+            os.remove(new_filename)
 
 
 def test_new_scalar_branch():
     original_file = uproot.open('tests/scalars_tree_file.root')
-    tree_name = 'tree'
-    original_tree = original_file[tree_name]
-    new_file_name = tempfile.mkstemp(suffix='.root', dir=os.getcwd())[1]
+    treename = 'tree'
+    original_tree = original_file[treename]
+    new_filename = tempfile.mkstemp(suffix='.root', dir=os.getcwd())[1]
     try:
         new_branch_dictionary = {'new_int_branch': np.array([5, 6])}
-        clone_tree(original_tree, new_file_name, new_branches=new_branch_dictionary)
-        new_file = uproot.open(new_file_name)
-        new_tree = new_file[tree_name]
+        clone_tree(original_tree, new_filename, new_branches=new_branch_dictionary)
+        new_file = uproot.open(new_filename)
+        new_tree = new_file[treename]
         assert new_tree['int_branch'].array().tolist() == [0, -1]
         assert new_tree['long_branch'].array().tolist() == [0, -2]
         assert np.allclose(new_tree['float_branch'].array(), [0.0, 3.3])
@@ -99,42 +99,42 @@ def test_new_scalar_branch():
         assert new_tree['bool_branch'].array().tolist() == [False, True]
         assert new_tree['new_int_branch'].array().tolist() == [5, 6]
     finally:
-        if os.path.isfile(new_file_name):
-            os.remove(new_file_name)
+        if os.path.isfile(new_filename):
+            os.remove(new_filename)
 
 
 def test_new_vector_branch():
     original_file = uproot.open('tests/vectors_tree_file.root')
-    tree_name = 'tree'
-    original_tree = original_file[tree_name]
-    new_file_name = tempfile.mkstemp(suffix='.root', dir=os.getcwd())[1]
+    treename = 'tree'
+    original_tree = original_file[treename]
+    new_filename = tempfile.mkstemp(suffix='.root', dir=os.getcwd())[1]
     try:
         new_branch_dictionary = {'new_int_vector_branch': awkward.fromiter([[1], [2, 3], []])}
-        clone_tree(original_tree, new_file_name, new_branches=new_branch_dictionary)
-        new_file = uproot.open(new_file_name)
-        new_tree = new_file[tree_name]
+        clone_tree(original_tree, new_filename, new_branches=new_branch_dictionary)
+        new_file = uproot.open(new_filename)
+        new_tree = new_file[treename]
         assert new_tree['int_vector_branch'].array().tolist() == [[], [-1, 2, 3], [13]]
         assert abs(new_tree['float_vector_branch'].array() - awkward.fromiter([[], [-7.7, 8.8, 9.9], [15.15]])).max().max() < 1e-5
         assert abs(new_tree['double_vector_branch'].array() - awkward.fromiter([[], [-10.10, 11.11, 12.12], [16.16]])).max().max() < 1e-5
         assert new_tree['new_int_vector_branch'].array().tolist() == [[1], [2, 3], []]
     finally:
-        if os.path.isfile(new_file_name):
-            os.remove(new_file_name)
+        if os.path.isfile(new_filename):
+            os.remove(new_filename)
 
 
 def test_select_branches():
     original_file = uproot.open('tests/scalars_tree_file.root')
-    tree_name = 'tree'
-    original_tree = original_file[tree_name]
-    new_file_name = tempfile.mkstemp(suffix='.root', dir=os.getcwd())[1]
+    treename = 'tree'
+    original_tree = original_file[treename]
+    new_filename = tempfile.mkstemp(suffix='.root', dir=os.getcwd())[1]
     try:
         branch_list = ['float_branch', 'bool_branch']
-        clone_tree(original_tree, new_file_name, branches=branch_list)
-        new_file = uproot.open(new_file_name)
-        new_tree = new_file[tree_name]
+        clone_tree(original_tree, new_filename, branches=branch_list)
+        new_file = uproot.open(new_filename)
+        new_tree = new_file[treename]
         assert set(map(lambda b: b.decode('utf-8'), new_tree.keys())) == set(branch_list)
         assert np.allclose(new_tree['float_branch'].array(), [0.0, 3.3])
         assert new_tree['bool_branch'].array().tolist() == [False, True]
     finally:
-        if os.path.isfile(new_file_name):
-            os.remove(new_file_name)
+        if os.path.isfile(new_filename):
+            os.remove(new_filename)
