1095a95171227dfd3d50071b6df1ac5d2067223c
Mason Proffitt <mproffit@cern.ch>
2020-08-20 22:06:01 +0200

    add setup.py #28

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='uproot_tree_utils',
+                 version=0.1,
+                 description='A small collection of utilities for handling ROOT TTrees with uproot.',
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 packages=setuptools.find_packages(exclude=['tests']),
+                 install_requires=['numpy', 'uproot'],
+                 author='Mason Proffitt',
+                 author_email='masonlp@uw.edu',
+                 url='https://github.com/masonproffitt/uproot_tree_utils')
