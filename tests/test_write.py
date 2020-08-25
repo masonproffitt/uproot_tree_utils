590060306b685aac86b95c51bbedfa953f074c6b
Mason Proffitt <mproffit@cern.ch>
2020-08-25 22:39:58 +0200

    add initial write_tree() test #5

---
+++ tests/test_write.py
@@ -0,0 +1,21 @@
+import tempfile
+import os
+
+import numpy as np
+
+from uproot_tree_utils import write_tree
+
+
+def test_write():
+    treename = 'tree'
+    branchname = 'branch'
+    original_array = np.array([1, 2, 3])
+    filename = tempfile.mkstemp(suffix='.root', dir=os.getcwd())[1]
+    try:
+        write_tree({branchname: original_array}, filename, treename)
+        file = uproot.open(filename)
+        tree = file[treename]
+        assert tree[branchname].array().tolist() == original_array.tolist()
+    finally:
+        if os.path.isfile(filename):
+            os.remove(filename)
